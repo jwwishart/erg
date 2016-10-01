fee92f1d341a692c9272691a710a4aa55b2fe83e
Justin Wishart <jwwishart@gmail.com>
2016-10-01 23:00:59 +1000

    Basic Token enum and structure

--- erg.cpp
+++ erg.cpp
@@ -1,5 +1,39 @@
 #include <stdio.h>
 
+// TODO in case I need to change for something like a unicode string? 
+//typedef char* string;
+
+enum TokenType {
+    TOKEN_UNKNOWN,
+
+    TOKEN_NUMBER,
+
+    TOKEN_OPERATOR_ADD,
+    TOKEN_OPERATOR_SUBTRACT,
+    TOKEN_OPERATOR_MULTIPLY,
+    TOKEN_OPERATOR_DIVIDE,
+};
+
+
+struct Token {
+    TokenType type;
+};
+
+
+// TODO 
+void lex(char *code) {
+    while (true) {
+        printf("test\n");
+        break;     
+    }
+
+    return;
+}
+
+
 int main() {
+    lex((char *)"Hello World!");
+
     printf("Hello World\n");
 }
+
